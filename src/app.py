6d8a0e88837cecd6f5d39763e1f9d2d2dd873d94
barret07 <149634146+barret07@users.noreply.github.com>
2024-04-01 01:08:29 +0000

    API_Lery

--- src/app.py
+++ src/app.py
@@ -1,15 +1,40 @@
-import os
-from sqlalchemy import create_engine
-import pandas as pd
-from dotenv import load_dotenv
+#FUNCION PARA CONECTAR CON WEB API SPOTIFY
 
-# load the .env file variables
+import os
+import seaborn as sns
+from dotenv import load_dotenv
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+# CONECTANDO SPOTIPY CON SERVER SPOTIFY
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+conex = spotipy.Spotify (auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret))
+
+# BUSCANDO ARTISTA "OASIS" E IMPORTANDO SU TOP 10 + TIEMPO + POPULARIDAD
+
+import pandas as pd
+
+my_favorite_artist = "2DaxqgrOhkeH0fpeiQq2f4"
+
+response = conex.artist_top_tracks ("2DaxqgrOhkeH0fpeiQq2f4")
+
+if response:
+    top_songs = response["tracks"]
+    top_songs = [{k: (format(v/(1000*60),".2f")) if k == "duration_ms" else v for k, v in 
+               track.items() if k in ["name", "popularity", "duration_ms"]} for track in top_songs]
+    
+top_songs_df = pd.DataFrame.from_records(top_songs)
+top_songs_df.sort_values (["popularity"], inplace = True)
+
+print (top_songs_df.head)
+
+# PLOTEANDO GRAFICO "POPULARITY vs. DURATION" PARA ANALISIS
+
+compare_plot = sns.scatterplot(data = top_songs_df, x = "popularity", y = "duration_ms")
+figure = compare_plot.get_figure()
+figure.savefig("scatter_plot.png")
